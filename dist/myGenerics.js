5187fec76593550e1e69e60aef9213b1eee653c3
Maverick18 <115613425+Aditya-Shandiya1812@users.noreply.github.com>
2023-08-05 16:52:21 +0530

    more on generics

--- dist/myGenerics.js
+++ dist/myGenerics.js
@@ -10,4 +10,47 @@ function identityTwo(val) {
 function identityThree(val) {
     return val;
 }
+function identityFour(val) {
+    return val;
+}
+function getSearchProduct(products) {
+    const index = 3;
+    return products[index];
+}
+function getSearchProductTwo(products) {
+    const index = 3;
+    return products[index];
+}
+const getSearchProductThree = (products) => {
+    const index = 3;
+    return products[index];
+};
+class Box {
+    constructor(initialValue) {
+        this.value = initialValue;
+    }
+    getValue() {
+        return this.value;
+    }
+    setValue(newValue) {
+        this.value = newValue;
+    }
+}
+const boxOfNumbers = new Box(42);
+const boxOfStrings = new Box("Hello, TypeScript!");
+console.log(boxOfNumbers.getValue());
+console.log(boxOfStrings.getValue());
+boxOfNumbers.setValue(100);
+boxOfStrings.setValue("Updated value");
+console.log(boxOfNumbers.getValue());
+console.log(boxOfStrings.getValue());
+class Pair {
+    constructor(first, second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+const pairOfStringAndNumber = new Pair("hello", 42);
+console.log(pairOfStringAndNumber.first);
+console.log(pairOfStringAndNumber.second);
 //# sourceMappingURL=myGenerics.js.map
--- src/myGenerics.ts
+++ src/myGenerics.ts
@@ -1,14 +1,71 @@
 const score: Array<number> = [];
 const names: Array<string> = [];
 
-function identityOne(val: boolean | number): boolean | number{
-    return val;
+function identityOne(val: boolean | number): boolean | number {
+  return val;
 }
 
-function identityTwo(val: any): any{
-    return val;
+function identityTwo(val: any): any {
+  return val;
 }
 
-function identityThree<Type>(val: Type): Type{
-    return val;
+function identityThree<Type>(val: Type): Type {
+  return val;
 }
+
+function identityFour<T>(val: T): T {
+  return val;
+}
+//identityThre and identityFour are exactly same
+
+function getSearchProduct<T>(products: Array<T>): T {
+  const index = 3;
+  return products[index];
+}
+
+function getSearchProductTwo<T>(products: T[]): T {
+  const index = 3;
+  return products[index];
+}
+
+const getSearchProductThree = <T>(products: T[]): T => {
+  const index = 3;
+  return products[index];
+};
+
+//Generic Classes
+class Box<T> {
+  private value: T;
+
+  constructor(initialValue: T) {
+    this.value = initialValue;
+  }
+
+  getValue(): T {
+    return this.value;
+  }
+
+  setValue(newValue: T): void {
+    this.value = newValue;
+  }
+}
+
+const boxOfNumbers = new Box<number>(42);
+const boxOfStrings = new Box<string>("Hello, TypeScript!");
+
+console.log(boxOfNumbers.getValue()); // Output: 42
+console.log(boxOfStrings.getValue()); // Output: Hello, TypeScript!
+
+boxOfNumbers.setValue(100);
+boxOfStrings.setValue("Updated value");
+
+console.log(boxOfNumbers.getValue()); // Output: 100
+console.log(boxOfStrings.getValue()); // Output: Updated value
+
+class Pair<T, U> {
+  constructor(public first: T, public second: U) {}
+}
+
+const pairOfStringAndNumber = new Pair<string, number>("hello", 42);
+console.log(pairOfStringAndNumber.first); // Output: hello
+console.log(pairOfStringAndNumber.second); // Output: 42
